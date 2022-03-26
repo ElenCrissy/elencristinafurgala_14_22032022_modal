5ab9b8f17e3bb816ef9efcb50012a3dcf1884e1e
Elencrissy <cristina.furgala@gmail.com>
2022-03-26 14:42:13 +0100

    Components package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Modal} from './Modal';
+import {Modal} from "./Modal.js";
 import reportWebVitals from './reportWebVitals';
 
 // ReactDOM.render(
